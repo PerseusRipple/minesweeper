a25d54e092140e7b069f7f372e00469e56dfca5f
PerseusRipple <perseus.ripple@gmail.com>
2019-02-25 17:49:42 -0500

    added onclick failed

--- src/App.js
+++ src/App.js
@@ -1,42 +1,65 @@
-import React, { Component } from 'react'
-
+import React, { Component } from "react";
+import axios from "axios";
 
 class App extends Component {
   state = {
-    easy: 0,
-    intermediate: 0,
-    hard: 0,
-    games: []
+    game: [[]]
   };
 
+  componentDidMount() {
+    axios
+      .post("https://minesweeper-api.herokuapp.com/games", { difficulty: 0 })
+      .then(resp => {
+        console.log({ resp });
+        this.setState({
+          game: resp.data.board
+        });
+      });
+  }
 
-
-
+  /*checkForBomb() {
+    axios
+    .get("https://minesweeper-api.herokuapp.com/games", { id: " " })
+      .then(resp => {
+        console.log({ resp });
+        this.setState({
+          game: resp.data.board
+        });
+      });
+    }
+  } */
 
   render() {
     return (
       <>
-      <header>
-        <h1>Ba BOOM!</h1>
-      </header>
-<section className="scoreboard">
-<section className="counter"></section>
-<section className="timer"></section>
-</section>
-    )
+        <section>
+          <header>
+            <h1>Ba BOOM!</h1>
+          </header>
+          <section className="scoreboard">
+            <section className="counter" />
+            <section className="timer" />
+          </section>
+        </section>
+
+        <section className="gameboard">
+          <table>
+            <tbody>
+              {this.state.game.map(row => {
+                return (
+                  <tr onClick={this.checkForBomb}>
+                    {row.map(col => {
+                      return <td>{col}</td>;
+                    })}
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </section>
+      </>
+    );
   }
 }
 
-<section className="gameboard">
-<table>
-  <tr> 
-    <td>
-      <button className="click_left_check">Check!</button>
-      <button className="click_right_flag">Flag</button>
-    </td>
-  </tr>
-
-</table>
-</section>
-
-export default App
+export default App;
