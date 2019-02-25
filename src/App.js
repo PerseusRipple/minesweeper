9020295af87496b7df9bb3af4edeabf9e0f3a948
PerseusRipple <perseus.ripple@gmail.com>
2019-02-25 14:49:25 -0500

    added buttons inside table

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
 import React, { Component } from 'react'
-import HelloWorld from './components/HelloWorld'
+
 
 class App extends Component {
+  state = {
+    easy: 0,
+    intermediate: 0,
+    hard: 0,
+    games: []
+  };
+
+
+
+
+
   render() {
-    return <HelloWorld />
+    return (
+      <>
+      <header>
+        <h1>Ba BOOM!</h1>
+      </header>
+<section className="scoreboard">
+<section className="counter"></section>
+<section className="timer"></section>
+</section>
+    )
   }
 }
+
+<section className="gameboard">
+<table>
+  <tr> 
+    <td>
+      <button className="click_left_check">Check!</button>
+      <button className="click_right_flag">Flag</button>
+    </td>
+  </tr>
+
+</table>
+</section>
 
 export default App
