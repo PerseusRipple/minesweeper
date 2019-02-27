97831ca3ae67ea232a15d3d40ff9dbef28ec6ff9
PerseusRipple <perseus.ripple@gmail.com>
2019-02-27 10:55:14 -0500

    added card component

---
+++ src/components/card.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class card extends Component {
+  render() {
+    return (
+      <div>
+        <button>Start Game</button>
+      </div>
+    );
+  }
+}
+
+export default card;
